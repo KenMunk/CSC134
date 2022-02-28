d2e0021fcea970006c058757987e850958bf8f23
Kenneth Munk <contact.me@kenmunk.com>
2022-02-27 20:22:49 -0800

    Update Kmunk--CSC134--Assignment_2--Q1.sql

--- Assignment_2/Kmunk--CSC134--Assignment_2--Q1.sql
+++ Assignment_2/Kmunk--CSC134--Assignment_2--Q1.sql
@@ -1,1 +1,59 @@
-select Fname, Minit, Lname, Address from Employee where Dno = 4
+#Question 1.1
+select 
+	concat(Fname, " ", Minit, ". ", Lname) as employee_name,  
+    Address 
+from 
+	Employee 
+where 
+	Dno
+    in
+    (select Dnumber from Department where Dname like "Administration")
+;
+
+#Question 1.2
+select 
+	concat(e.Fname, " ", e.Minit, ". ", e.Lname) as Employee_Name, 
+    concat(s.Fname, " ", s.Minit, ". ", s.Lname) as Supervisor_Name
+from 
+	Employee as e,
+    Employee as s
+where 
+	s.ssn = e.super_ssn
+;
+
+#Question 1.3
+select 
+	Salary as original_Salary,
+	Salary*1.5 as new_Salary
+from 
+	Employee 
+where
+	ssn
+	in 
+	(Select 
+		Essn
+	 from
+		Works_on
+	 where
+		Pno 
+		in
+		(Select 
+			Pnumber
+		from
+			Project
+		where
+			Pname like "Newbenefits"
+		)
+	)
+;
+
+#Question 1.4
+select distinct
+	Salary as Salaries_in_Houston
+from
+	Employee
+where
+	Address like "%Houston, TX%"
+;
+    
+				
